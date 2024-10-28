805a7a9d6abaf7c1e7a4ca3a3c36a3c22d9bc7dd
ashjianda <129344873+ashjianda@users.noreply.github.com>
2024-10-28 03:52:56 +0000

    commit

--- Question 1/semaphores.c
+++ Question 1/semaphores.c
@@ -2,127 +2,104 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#define SIZE 5
-#define NUMB_THREADS 6
-#define PRODUCER_LOOPS 2
-typedef int buffer_t;
-buffer_t buffer[SIZE];
-int buffer_index;
-pthread_mutex_t buffer_mutex;
-/* initially buffer will be empty. full_sem
-will be initialized to buffer SIZE, which means
-SIZE number of producer threads can write to it.
-And empty_sem will be initialized to 0, so no
-consumer can read from buffer until a producer
-thread posts to empty_sem */
-sem_t full_sem;  /* when 0, buffer is full */
-sem_t empty_sem; /* when 0, buffer is empty. Kind of
-like an index for the buffer */
-void insertbuffer(buffer_t value)
-{
-    if (buffer_index < SIZE)
-    {
-        buffer[buffer_index++] = value;
-    }
-    else
-    {
-        printf("Buffer overflow\n");
-    }
-}
-buffer_t dequeuebuffer()
-{
-    if (buffer_index > 0)
-    {
-        return buffer[--buffer_index]; // buffer_index-- would be error!
-    }
-    else
-    {
-        printf("Buffer underflow\n");
-    }
-    return 0;
-}
-void *producer(void *thread_n)
-{
-    int thread_numb = *(int *)thread_n;
-    buffer_t value;
-    int i = 0;
-    while (i++ < PRODUCER_LOOPS)
-    {
-        sleep(rand() % 10);
-        value = rand() % 100;
-        sem_wait(&full_sem); // sem=0: wait. sem>0: go and decrement it
-        /* possible race condition here. After this thread wakes up,
-        another thread could aqcuire mutex before this one, and add to list.
-        Then the list would be full again
-        and when this thread tried to insert to buffer there would be
-        a buffer overflow error */
-        pthread_mutex_lock(&buffer_mutex); /* protecting critical section */
-        insertbuffer(value);
-        pthread_mutex_unlock(&buffer_mutex);
-        sem_post(&empty_sem); // post (increment) emptybuffer semaphore
-        printf("Producer %d added %d to buffer\n", thread_numb, value);
-    }
+#include <unistd.h>
+
+#define MAX_BUFFER_SIZE 1000
+
+int *input_buffer, *output_buffer;
+int input_index = 0, copy_index = 0, output_index = 0, consume_index = 0;
+int buffer_size;
+
+pthread_mutex_t input_mutex, output_mutex;
+sem_t producer_done, copier_done;
+
+void *producer(void *arg) {
+    int value = rand() % 100;
+    pthread_mutex_lock(&input_mutex);
+    input_buffer[input_index++] = value;
+    printf("Inserted %d\n", value);
+    pthread_mutex_unlock(&input_mutex);
+    sem_post(&producer_done);
     pthread_exit(0);
 }
-void *consumer(void *thread_n)
-{
-    int thread_numb = *(int *)thread_n;
-    buffer_t value;
-    int i = 0;
-    while (i++ < PRODUCER_LOOPS)
-    {
-        sem_wait(&empty_sem);
-        /* there could be race condition here, that could cause
-        buffer underflow error */
-        pthread_mutex_lock(&buffer_mutex);
-        value = dequeuebuffer(value);
-        pthread_mutex_unlock(&buffer_mutex);
-        sem_post(&full_sem); // post (increment) fullbuffer semaphore
-        printf("Consumer %d dequeue %d from buffer\n", thread_numb, value);
-    }
+
+void *copier(void *arg) {
+    sem_wait(&producer_done); // Ensure that producer has added an item
+    
+    pthread_mutex_lock(&input_mutex);
+    int value = input_buffer[copy_index++];
+    pthread_mutex_unlock(&input_mutex);
+    
+    pthread_mutex_lock(&output_mutex);
+    output_buffer[output_index++] = value;
+    pthread_mutex_unlock(&output_mutex);
+    sem_post(&copier_done);
     pthread_exit(0);
 }
-int main(int argc, int **argv)
-{
-    buffer_index = 0;
-    pthread_mutex_init(&buffer_mutex, NULL);
-    sem_init(&full_sem, // sem_t *sem
-             0,         // int pshared. 0 = shared between threads of process, 1 = shared
-             between processes
-                 SIZE); // unsigned int value. Initial value
-    sem_init(&empty_sem,
-             0,
-             0);
-    /* full_sem is initialized to buffer size because SIZE number of
-    producers can add one element to buffer each. They will wait
-    semaphore each time, which will decrement semaphore value.
-    empty_sem is initialized to 0, because buffer starts empty and
-    consumer cannot take any element from it. They will have to wait
-    until producer posts to that semaphore (increments semaphore
-    value) */
-    pthread_t thread[NUMB_THREADS];
-    int thread_numb[NUMB_THREADS];
-    int i;
-    for (i = 0; i < NUMB_THREADS;)
-    {
-        thread_numb[i] = i;
-        pthread_create(thread + i,       // pthread_t *t
-                       NULL,             // const pthread_attr_t *attr
-                       producer,         // void *(*start_routine) (void *)
-                       thread_numb + i); // void *arg
-        i++;
-        thread_numb[i] = i;
-        // playing a bit with thread and thread_numb pointers...
-        pthread_create(&thread[i],       // pthread_t *t
-                       NULL,             // const pthread_attr_t *attr
-                       consumer,         // void *(*start_routine) (void *)
-                       &thread_numb[i]); // void *arg
-        i++;
+
+void *consumer(void *arg) {
+    sem_wait(&copier_done); // Ensure that copier has added an item
+    
+    pthread_mutex_lock(&output_mutex);
+    int value = output_buffer[consume_index++];
+    pthread_mutex_unlock(&output_mutex);
+    printf("Consumed %d\n", value);    
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: ./semaphores <buffer_size>\n");
+        exit(1);
     }
-    for (i = 0; i < NUMB_THREADS; i++)
-        pthread_join(thread[i], NULL);
-    pthread_mutex_destroy(&buffer_mutex);
-    sem_destroy(&full_sem);
-    sem_destroy(&empty_sem);
+
+    buffer_size = atoi(argv[1]);
+    if (buffer_size > MAX_BUFFER_SIZE || buffer_size <= 0) {
+        printf("Invalid buffer size. Exiting.\n");
+        exit(1);
+    }
+
+    input_buffer = (int *)malloc(buffer_size * sizeof(int));
+    output_buffer = (int *)malloc(buffer_size * sizeof(int));
+
+    pthread_mutex_init(&input_mutex, NULL);
+    pthread_mutex_init(&output_mutex, NULL);
+    sem_init(&producer_done, 0, 0);
+    sem_init(&copier_done, 0, 0);
+
+    int num_threads = buffer_size;
+    pthread_t producers[num_threads], copiers[num_threads], consumers[num_threads];
+    printf("The buffer size is %d\n", buffer_size);
+
+    // Start producer threads
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&producers[i], NULL, producer, NULL);
+    }
+
+    // Start copier threads after all producers have started
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&copiers[i], NULL, copier, NULL);
+    }
+
+    // Start consumer threads after all copiers have started
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&consumers[i], NULL, consumer, NULL);
+    }
+
+    // Wait for all threads to complete
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(producers[i], NULL);
+        pthread_join(copiers[i], NULL);
+        pthread_join(consumers[i], NULL);
+    }
+
+    pthread_mutex_destroy(&input_mutex);
+    pthread_mutex_destroy(&output_mutex);
+    sem_destroy(&producer_done);
+    sem_destroy(&copier_done);
+
+    free(input_buffer);
+    free(output_buffer);
+
     return 0;
 }
